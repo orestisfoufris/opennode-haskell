8452ec0b2af0d7beec167066ce4badf1c37d4504
orestisfoufris <orestis.foufris@gmail.com>
2019-01-09 12:15:22 +0000

    formatting files

--- app/Data.hs
+++ app/Data.hs
@@ -3,19 +3,19 @@
 module Data where
 
 data WithdrawalsReq a = WithdrawalsReq
-  { wtype :: WithdrawalType
-  , amount :: a
+  { wtype   :: WithdrawalType
+  , amount  :: a
   , address :: String
   } deriving (Show, Eq)
 
 data WithdrawalsResp a = WithdrawalsResp
-  { id :: String
-  , wtype :: WithdrawalType
-  , amount :: a
-  , reference :: String
+  { id          :: String
+  , wtype       :: WithdrawalType
+  , amount      :: a
+  , reference   :: String
   , processedAt :: String
-  , address :: String
-  , fee :: a
+  , address     :: String
+  , fee         :: a
   } deriving (Show, Eq)
 
 data WithdrawalType
@@ -24,5 +24,5 @@ data WithdrawalType
   deriving (Eq)
 
 instance Show WithdrawalType where
-  show LN = "ln"
+  show LN    = "ln"
   show CHAIN = "chain"
--- app/Main.hs
+++ app/Main.hs
@@ -1,37 +1,38 @@
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 
 module Main where
 
-import Control.Exception (throwIO)
-import Control.Monad
-import Control.Monad.IO.Class
-import qualified Data.ByteString.Char8 as B
-import Data.Maybe (fromJust)
-import Data.Monoid ((<>))
-import Data.Aeson
-import qualified Data.Text as T
-import GHC.Generics
-import Network.HTTP.Req
+import           Control.Exception      (throwIO)
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Aeson
+import qualified Data.ByteString.Char8  as B
+import           Data.Maybe             (fromJust)
+import           Data.Monoid            ((<>))
+import qualified Data.Text              as T
+import           GHC.Generics
+import           Network.HTTP.Req
 
 instance MonadHttp IO where
   handleHttpException = throwIO
 
 data Resp = Resp
-  { success  :: String
+  { success :: String
   , message :: String
   } deriving (Show, Generic, ToJSON, FromJSON)
 
 main :: IO ()
 main =
    do
-    r <- getWithdrawal "dev-api.opennode.co" "/v1/withdrawal/" "123" "token_here"
-    liftIO $ print (responseBody r :: Value)
+    r <- getWithdrawal "dev-api.opennode.co" "/v1/withdrawal/" "123" "1fbcdbb7-ad6d-4487-ba07-974542fdb497"
+    print (responseBody r :: Value)
 
-getWithdrawal baseUrl path withId token = 
+getWithdrawal baseUrl path withId token =
   req
     GET
-    (https baseUrl /: (T.pack $ (++) path withId))
+    (https baseUrl /: T.pack ((++) path withId))
     NoReqBody
     jsonResponse
     (header "Authorization" token <> header "Accept" "application/json")
