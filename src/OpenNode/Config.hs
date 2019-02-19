76864d4bdc247002d09a006e612a03f91a18a802
Orestis Foufris <orestis.foufris@gmail.com>
2019-02-19 23:11:48 +0000

    remove tokens (they have been rotated).

--- src/OpenNode/Config.hs
+++ src/OpenNode/Config.hs
@@ -15,14 +15,16 @@ data Config = Config
     , configUrl   :: !Text
     }
 
+-- TODO: tokens should come from ENVS
+
 prodToken :: ByteString
-prodToken = "81822829-ee7f-4024-8772-b1bfbb4a7123"
+prodToken = "insert prod token here"
 
 prodUrl :: Text
 prodUrl = "api.opennode.co"
 
 devToken :: ByteString
-devToken = "1f021a7b-6011-473e-b857-58d77d04b39c"
+devToken = "insert dev token here"
 
 devUrl :: Text
 devUrl = "dev-api.opennode.co"
