e86e7e80dc5f8cb7c65acde48688078762633411
Orestis <orestis.foufris@hotmail.com>
2019-10-12 22:39:22 +0100

    fix URL paths

--- src/OpenNode/Api.hs
+++ src/OpenNode/Api.hs
@@ -37,12 +37,11 @@ withdrawalInfo cfg wid = responseBody <$> req
 withdrawals :: (MonadHttp m) => Config -> m (ResponseData [Withdrawal])
 withdrawals cfg = responseBody <$> req
   GET
-  (https baseUrl /: path)
+  (https baseUrl /: "v1" /: "withdrawals")
   NoReqBody
   jsonResponse
   (header "Authorization" token <> header "Content-Type" "application/json")
  where
-  path    = "/v1/withdrawals"
   baseUrl = configUrl cfg
   token   = configToken cfg
 
@@ -52,31 +51,29 @@ initiateWithdrawal cfg wreq =
   responseBody <$>
   req
     POST
-    (https baseUrl /: path)
+    (https baseUrl /: "v2" /: "withdrawals")
     (ReqBodyJson wreq)
     jsonResponse
     (header "Authorization" token)
   where
-    path = "/v2/withdrawals"
     baseUrl = configUrl cfg
     token = configToken cfg
 
 charges :: (MonadHttp m) => Config -> m (ResponseData [Charge])
 charges cfg = responseBody <$> req
   GET
-  (https baseUrl /: path)
+  (https baseUrl /: "v1" /: "charges")
   NoReqBody
   jsonResponse
   (header "Authorization" token)
   where
-    path = "/v1/charges"
     baseUrl = configUrl cfg
     token = configToken cfg
 
 chargeInfo:: (MonadHttp m) => Config -> Text -> m (ResponseData Charge)
 chargeInfo cfg cid = responseBody <$> req
   GET
-  (https baseUrl /: path /: cid)
+  (https baseUrl /: "v1" /: "charge" /: cid)
   NoReqBody
   jsonResponse
   (header "Authorization" token)
@@ -89,7 +86,7 @@ createCharge :: (MonadHttp m) => Config -> ChargeRequest
              -> m (ResponseData ChargeResponse)
 createCharge cfg creq = responseBody <$> req
   POST
-  (https baseUrl /: path)
+  (https baseUrl /: "v1" /: "charges")
   (ReqBodyJson creq)
   jsonResponse
   (header "Authorization" token)
@@ -101,19 +98,18 @@ createCharge cfg creq = responseBody <$> req
 accountBalance :: (MonadHttp m) => Config -> m (ResponseData Balance)
 accountBalance cfg = responseBody <$> req
   GET
-  (https baseUrl /: path)
+  (https baseUrl /: "v1" /: "account" /: "balance")
   NoReqBody
   jsonResponse
   (header "Authorization" token)
   where
-    path = "/v1/account/balance"
     baseUrl = configUrl cfg
     token = configToken cfg
 
 exchangeRates :: (MonadHttp m) => Config -> m (ResponseData Rates)
 exchangeRates cfg = responseBody <$> req
   GET
-  (https baseUrl /: path)
+  (https baseUrl /: "v1" /: "rates")
   NoReqBody
   jsonResponse
   (header "Authorization" token)
@@ -125,23 +121,21 @@ exchangeRates cfg = responseBody <$> req
 supportedCurrencies :: (MonadHttp m) => Config -> m (ResponseData [Text])
 supportedCurrencies cfg = responseBody <$> req
   GET
-  (https baseUrl /: path)
+  (https baseUrl /: "v1" /: "currencies")
   NoReqBody
   jsonResponse
   (header "Authorization" token)
   where
-    path = "/v1/currencies"
     baseUrl = configUrl cfg
     token = configToken cfg
 
 listRefunds :: (MonadHttp m) => Config -> m (ResponseData [Refund])
 listRefunds cfg = responseBody <$> req
   GET
-  (https baseUrl /: path)
+  (https baseUrl /: "v1" /: "refunds")
   NoReqBody
   jsonResponse
   (header "Authorization" token)
   where
-    path = "/v1/refunds"
     baseUrl = configUrl cfg
     token = configToken cfg
